8b0f84b6620b8be11373d32f4028d9320c238a19
teak <teaksoftware@gmail.com>
2015-02-07 10:47:45 -0800

    Initial commit 2.0

---
+++ PusshCap/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace PusshCap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "cap.png";
+            if (args.Length != 0)
+            {
+                name = args[0];
+            }
+
+            Rectangle bounds = Screen.GetBounds(Cursor.Position);
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(bounds.Location, Point.Empty, bounds.Size);
+                }
+                bitmap.Save(name, ImageFormat.Png);
+            }
+
+            string output = bounds.X.ToString() + ",";
+            output += bounds.Y.ToString() + ",";
+            output += bounds.Width.ToString() + ",";
+            output += bounds.Height.ToString() + ",";
+            output += Cursor.Position.X.ToString() + ",";
+            output += Cursor.Position.Y.ToString() + ",";
+            output += name;
+
+            System.Console.WriteLine(output);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # PusshCap
-Simple command line exe to capture screenshots
+Simple command line exe to capture screenshots.
+
+There is a compiled 64 bit exe for convenience.
+
+Captures the screen that the mouse cursor is on to the file specified as the only command line argument. If there are no args, it will save to 'cap.png' in the same directory as the exe. Always saves as PNG.
+
+Outputs one line to stdout. A comma separated list of useful info about the screen of which the shot was taken:
+
+screen-bounds-x,screen-bounds-y,screen-bounds-width,screen-bounds-height,mouse-x,mouse-y,filename-or-path
+
+The screen bounds are taken directly from a .NET rectangle and are relative to the main screen. They can be negative, depending on screen orientation.
+
+https://msdn.microsoft.com/en-us/library/system.drawing.rectangle%28v=vs.110%29.aspx 
+
+MIT License
+
